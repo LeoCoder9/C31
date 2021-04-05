cb1f11ac939f7707d73b83cc434a5655d46a40d5
leocoder9 <leandroralphperez@gmail.com>
2021-04-05 18:18:48 +0100

    31

--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var sling1
+var gameState = "onsling"
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+    
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
 
     // created platform
     ground = new Ground(600,height,1200,20);
@@ -23,7 +28,6 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -34,16 +38,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,30);
+
+    
+    sling1 = new sling(bird.body,{x:200, y:70})
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -62,4 +66,23 @@ function draw(){
     bird.display();
     //displayed platform
     platform.display();
+
+
+    sling1.display()
+
+
+}
+function mouseDragged(){
+    if(gameState === "onsling")
+    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+
+}
+function mouseReleased(){
+    sling1.fly()
+    gameState = "launch"
+}
+function keyPressed(){
+    if (keyCode === 32){
+      // sling1.reset(bird.body)
+    }
 }
